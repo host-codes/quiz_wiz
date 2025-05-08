85f359bba8f672e668d98874aebd76becb3126ab
host-codes <110379028+host-codes@users.noreply.github.com>
2025-05-09 02:52:17 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-//const { transporter } = require('../server');
+const { transporter } = require('../server');
 const transporter = require('../config/email');
 const User = require('../models/User');
 
