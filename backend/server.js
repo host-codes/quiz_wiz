11052742d9941368d943c83da8ff26f5ce61d9cc
host-codes <110379028+host-codes@users.noreply.github.com>
2025-05-09 09:28:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,8 +42,8 @@ app.use(bodyParser.json());
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+//.then(() => console.log('Connected to MongoDB'))
+//.catch(err => console.error('MongoDB connection error:', err));
 
 // Email transporter
 /*const transporter = nodemailer.createTransport({
